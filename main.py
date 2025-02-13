6a51e9fcca0e07cc35131664528b630276e3c22f
Milo Johnson <75079687+Milo-Johnson@users.noreply.github.com>
2025-02-13 13:51:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,7 @@ import docker
 import schedule
 import time
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Welcome to stAItus-test!</p>"
-
 if __name__ == "__main__":
-
-    print("Monitoring started. Press Ctrl+C to stop.")
-
-    port = int(os.environ.get('PORT', 5003))
-    app.run(debug=False, host='0.0.0.0', port=port)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-        
-        print("Container is running... will crash in 5 seconds.")
-        time.sleep(5)
-        raise RuntimeError("Intentional crash for testing purposes.")
+    print("Container is running... will crash in 5 seconds.")
+    time.sleep(5)
+    raise RuntimeError("Intentional crash for testing purposes.")
